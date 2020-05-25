409de8d8d1e68fe4539f2ffd67260af75d3ed4e3
Marina Miyaoka <miyaokamarina@gmail.com>
2020-05-26 02:29:43 +0300

    Updated bounded again.

--- src/math/layer/layer_bounded.rs
+++ src/math/layer/layer_bounded.rs
@@ -4,21 +4,21 @@ use crate::*;
 use std::cmp::*;
 
 pub trait LayerBounded<T> {
-    fn min_max_element(&self) -> (&T, &T);
+    fn min_max(&self) -> (T, T);
 }
 
 impl<T: PartialOrd + Bounded> LayerBounded<T> for [T] {
-    fn min_max_element(&self) -> (&T, &T) {
-        let mut min = T::max_bound();
-        let mut max = T::min_bound();
+    fn min_max(&self) -> (T, T) {
+        let mut min = T::MAX_BOUND;
+        let mut max = T::MIN_BOUND;
 
         for i in self {
-            if i < min {
-                min = i;
+            if *i < min {
+                min = *i;
             }
 
-            if i > max {
-                max = i;
+            if *i > max {
+                max = *i;
             }
         }
 

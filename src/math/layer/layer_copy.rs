fcc72260e86598c824494c4b098bc300cb9d1235
Marina Miyaoka <miyaokamarina@gmail.com>
2020-05-26 02:30:29 +0300

    Updated LayerCopy.

--- src/math/layer/layer_copy.rs
+++ src/math/layer/layer_copy.rs
@@ -1,19 +1,19 @@
 #![allow(dead_code)]
 
-pub trait LayerCopy<T: Clone> {
+pub trait LayerCopy<T> {
     /// For each $M_i$ in the $M$, it takes $S_{F_i}$, and copies it to $S_i$.
     ///
     /// Each $M_i \in [0, L_F)$, where $L_F$ is the length of the $F$.
     ///
     /// $L_M <= L_S, L_M <= L_F$, where $L_M$ it the length of the $M$, $L_S$ is the length of the $S$.
     ///
-    /// | $F$    | $M$    | $S$    |
-    /// | :----: | :----: | :----: |
-    /// | $8$    | $1$    | $1$    |
-    /// | $1$    | $2$    | $4$    |
-    /// | $4$    | $0$    | $8$    |
-    /// | $8$    | $3$    | $8$    |
-    /// | $9$    |        |        |
+    /// | $F$           | $M$                       | $S$    |
+    /// | :-----------: | :-----------------------: | :----: |
+    /// | $8$           | $4$ — from $F_4$ to $S_0$ | $1$    |
+    /// | $\varnothing$ | $2$ — from $F_2$ to $S_1$ | $4$    |
+    /// | $4$           | $0$ — from $F_0$ to $S_2$ | $8$    |
+    /// | $8$           | $3$ — from $F_3$ to $S_3$ | $8$    |
+    /// | $1$           |                           |        |
     ///
     /// # Arguments
     ///
@@ -23,7 +23,7 @@ pub trait LayerCopy<T: Clone> {
     fn copy_from_slice_by_mapping(&mut self, mapping: &[usize], from: &Self);
 }
 
-impl<T: Copy> LayerCopy<T> for &mut [T] {
+impl<T: Copy> LayerCopy<T> for [T] {
     fn copy_from_slice_by_mapping(&mut self, mapping: &[usize], from: &Self) {
         for (i, j) in mapping.iter().enumerate() {
             self[i] = from[*j];

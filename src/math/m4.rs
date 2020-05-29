0517dddd96f14ee4f7265dc7c81b1904eef04e70
Lain-dono <lain.dono@gmail.com>
2020-05-29 22:47:25 +0300

    Refactor M3, M4, V3 and V4

--- src/math/m4.rs
+++ src/math/m4.rs
@@ -2,7 +2,7 @@
 
 use crate::*;
 
-#[derive(Debug, Copy, Clone, Default)]
+#[derive(Clone, Copy, PartialEq, Debug)]
 pub struct M4 {
     pub a: V4,
     pub b: V4,
@@ -10,18 +10,32 @@ pub struct M4 {
     pub d: V4,
 }
 
-impl M4 {
-    pub fn new(a: f32, b: f32, c: f32, d: f32, e: f32, f: f32, g: f32, h: f32, i: f32, j: f32, k: f32, l: f32, m: f32, n: f32, o: f32, p: f32) -> M4 {
-        M4 {
-            a: V4::new(a, b, c, d),
-            b: V4::new(e, f, g, h),
-            c: V4::new(i, j, k, l),
-            d: V4::new(m, n, o, p),
+impl Default for M4 {
+    fn default() -> Self {
+        Self::identity()
+    }
+}
+
+impl From<[[f32; 4]; 4]> for M4 {
+    fn from([a, b, c, d]: [[f32; 4]; 4]) -> Self {
+        Self {
+            a: a.into(),
+            b: b.into(),
+            c: c.into(),
+            d: d.into(),
         }
     }
+}
 
-    pub fn identity() -> M4 {
-        M4 {
+impl Into<[[f32; 4]; 4]> for M4 {
+    fn into(self) -> [[f32; 4]; 4] {
+        [self.a.into(), self.b.into(), self.c.into(), self.d.into()]
+    }
+}
+
+impl M4 {
+    pub const fn identity() -> Self {
+        Self {
             a: V4::unit_x(),
             b: V4::unit_y(),
             c: V4::unit_z(),
@@ -29,128 +43,63 @@ impl M4 {
         }
     }
 
-    pub fn invert(&self, result: &mut M4) {
-        let a: f32 = self.a.x;
-        let b: f32 = self.a.y;
-        let c: f32 = self.a.z;
-        let d: f32 = self.a.w;
+    pub fn invert(&self, output: &mut Self) {
+        let Self {
+            a: V4 { x: a, y: b, z: c, w: d },
+            b: V4 { x: e, y: f, z: g, w: h },
+            c: V4 { x: i, y: j, z: k, w: l },
+            d: V4 { x: m, y: n, z: o, w: p },
+        } = *self;
 
-        let e: f32 = self.b.x;
-        let f: f32 = self.b.y;
-        let g: f32 = self.b.z;
-        let h: f32 = self.b.w;
+        let α = a * f - b * e;
+        let β = a * g - c * e;
+        let γ = a * h - d * e;
+        let δ = b * g - c * f;
+        let ε = b * h - d * f;
+        let ζ = c * h - d * g;
+        let η = i * n - j * m;
+        let θ = i * o - k * m;
+        let ι = i * p - l * m;
+        let κ = j * o - k * n;
+        let λ = j * p - l * n;
+        let μ = k * p - l * o;
 
-        let i: f32 = self.c.x;
-        let j: f32 = self.c.y;
-        let k: f32 = self.c.z;
-        let l: f32 = self.c.w;
+        let inv_det = (α * μ - β * λ + γ * κ + δ * ι - ε * θ + ζ * η).recip();
 
-        let m: f32 = self.d.x;
-        let n: f32 = self.d.y;
-        let o: f32 = self.d.z;
-        let p: f32 = self.d.w;
+        output.a.x = (f * μ - g * λ + h * κ) * inv_det;
+        output.a.y = (c * λ - b * μ - d * κ) * inv_det;
+        output.a.z = (n * ζ - o * ε + p * δ) * inv_det;
+        output.a.w = (k * ε - j * ζ - l * δ) * inv_det;
 
-        let α: f32 = a * f - b * e;
-        let β: f32 = a * g - c * e;
-        let γ: f32 = a * h - d * e;
-        let δ: f32 = b * g - c * f;
-        let ε: f32 = b * h - d * f;
-        let ζ: f32 = c * h - d * g;
-        let η: f32 = i * n - j * m;
-        let θ: f32 = i * o - k * m;
-        let ι: f32 = i * p - l * m;
-        let κ: f32 = j * o - k * n;
-        let λ: f32 = j * p - l * n;
-        let μ: f32 = k * p - l * o;
+        output.b.x = (g * ι - e * μ - h * θ) * inv_det;
+        output.b.y = (a * μ - c * ι + d * θ) * inv_det;
+        output.b.z = (o * γ - m * ζ - p * β) * inv_det;
+        output.b.w = (i * ζ - k * γ + l * β) * inv_det;
 
-        let inv_det: f32 = 1.0 / (α * μ - β * λ + γ * κ + δ * ι - ε * θ + ζ * η);
+        output.c.x = (e * λ - f * ι + h * η) * inv_det;
+        output.c.y = (b * ι - a * λ - d * η) * inv_det;
+        output.c.z = (m * ε - n * γ + p * α) * inv_det;
+        output.c.w = (j * γ - i * ε - l * α) * inv_det;
 
-        result.a.x = (f * μ - g * λ + h * κ) * inv_det;
-        result.a.y = (c * λ - b * μ - d * κ) * inv_det;
-        result.a.z = (n * ζ - o * ε + p * δ) * inv_det;
-        result.a.w = (k * ε - j * ζ - l * δ) * inv_det;
-
-        result.b.x = (g * ι - e * μ - h * θ) * inv_det;
-        result.b.y = (a * μ - c * ι + d * θ) * inv_det;
-        result.b.z = (o * γ - m * ζ - p * β) * inv_det;
-        result.b.w = (i * ζ - k * γ + l * β) * inv_det;
-
-        result.c.x = (e * λ - f * ι + h * η) * inv_det;
-        result.c.y = (b * ι - a * λ - d * η) * inv_det;
-        result.c.z = (m * ε - n * γ + p * α) * inv_det;
-        result.c.w = (j * γ - i * ε - l * α) * inv_det;
-
-        result.d.x = (f * θ - e * κ - g * η) * inv_det;
-        result.d.y = (a * κ - b * θ + c * η) * inv_det;
-        result.d.z = (n * β - m * δ - o * α) * inv_det;
-        result.d.w = (i * δ - j * β + k * α) * inv_det;
+        output.d.x = (f * θ - e * κ - g * η) * inv_det;
+        output.d.y = (a * κ - b * θ + c * η) * inv_det;
+        output.d.z = (n * β - m * δ - o * α) * inv_det;
+        output.d.w = (i * δ - j * β + k * α) * inv_det;
     }
 
-    pub fn mul_m4(&mut self, other: &M4) {
-        let a: f32 = self.a.x;
-        let b: f32 = self.a.y;
-        let c: f32 = self.a.z;
-        let d: f32 = self.a.w;
+    pub fn mul_m4(&mut self, other: &Self) {
+        let x = V4::new(self.a.x, self.b.x, self.c.x, self.d.x);
+        let y = V4::new(self.a.y, self.b.y, self.c.y, self.d.y);
+        let z = V4::new(self.a.z, self.b.z, self.c.z, self.d.z);
+        let w = V4::new(self.a.w, self.b.w, self.c.w, self.d.w);
 
-        let e: f32 = self.b.x;
-        let f: f32 = self.b.y;
-        let g: f32 = self.b.z;
-        let h: f32 = self.b.w;
-
-        let i: f32 = self.c.x;
-        let j: f32 = self.c.y;
-        let k: f32 = self.c.z;
-        let l: f32 = self.c.w;
-
-        let m: f32 = self.d.x;
-        let n: f32 = self.d.y;
-        let o: f32 = self.d.z;
-        let p: f32 = self.d.w;
-
-        let α: f32 = other.a.x;
-        let β: f32 = other.a.y;
-        let γ: f32 = other.a.z;
-        let δ: f32 = other.a.w;
-
-        self.a.x = α * a + β * e + γ * i + δ * m;
-        self.a.y = α * b + β * f + γ * j + δ * n;
-        self.a.z = α * c + β * g + γ * k + δ * o;
-        self.a.w = α * d + β * h + γ * l + δ * p;
-
-        let α: f32 = other.b.x;
-        let β: f32 = other.b.y;
-        let γ: f32 = other.b.z;
-        let δ: f32 = other.b.w;
-
-        self.b.x = α * a + β * e + γ * i + δ * m;
-        self.b.y = α * b + β * f + γ * j + δ * n;
-        self.b.z = α * c + β * g + γ * k + δ * o;
-        self.b.w = α * d + β * h + γ * l + δ * p;
-
-        let α: f32 = other.c.x;
-        let β: f32 = other.c.y;
-        let γ: f32 = other.c.z;
-        let δ: f32 = other.c.w;
-
-        self.c.x = α * a + β * e + γ * i + δ * m;
-        self.c.y = α * b + β * f + γ * j + δ * n;
-        self.c.z = α * c + β * g + γ * k + δ * o;
-        self.c.w = α * d + β * h + γ * l + δ * p;
-
-        let α: f32 = other.d.x;
-        let β: f32 = other.d.y;
-        let γ: f32 = other.d.z;
-        let δ: f32 = other.d.w;
-
-        self.d.x = α * a + β * e + γ * i + δ * m;
-        self.d.y = α * b + β * f + γ * j + δ * n;
-        self.d.z = α * c + β * g + γ * k + δ * o;
-        self.d.w = α * d + β * h + γ * l + δ * p;
+        self.a = V4::new(other.a.dot(x), other.a.dot(y), other.a.dot(z), other.a.dot(w));
+        self.b = V4::new(other.b.dot(x), other.b.dot(y), other.b.dot(z), other.b.dot(w));
+        self.c = V4::new(other.c.dot(x), other.c.dot(y), other.c.dot(z), other.c.dot(w));
+        self.d = V4::new(other.d.dot(x), other.d.dot(y), other.d.dot(z), other.d.dot(w));
     }
 
     pub fn get_translation_v3(&self) -> &V3 {
-        let v3 = &self.d as *const V4 as usize as *const V3;
-
-        unsafe { &*v3 }
+        unsafe { &*(&self.d as *const V4 as *const V3) }
     }
 }
--- src/math/v4.rs
+++ src/math/v4.rs
@@ -1,6 +1,10 @@
 #![allow(dead_code)]
 
-#[derive(Debug, Copy, Clone, Default)]
+use crate::*;
+
+/// A 4D vector.
+#[derive(Clone, Copy, PartialEq, Debug, Default)]
+#[repr(align(16))]
 pub struct V4 {
     pub x: f32,
     pub y: f32,
@@ -8,44 +12,157 @@ pub struct V4 {
     pub w: f32,
 }
 
+impl From<[f32; 4]> for V4 {
+    fn from([x, y, z, w]: [f32; 4]) -> Self {
+        Self { x, y, z, w }
+    }
+}
+
+impl From<(f32, f32, f32, f32)> for V4 {
+    fn from((x, y, z, w): (f32, f32, f32, f32)) -> Self {
+        Self { x, y, z, w }
+    }
+}
+
+impl Into<[f32; 4]> for V4 {
+    fn into(self) -> [f32; 4] {
+        [self.x, self.y, self.z, self.w]
+    }
+}
+
+impl Into<(f32, f32, f32, f32)> for V4 {
+    fn into(self) -> (f32, f32, f32, f32) {
+        (self.x, self.y, self.z, self.w)
+    }
+}
+
+impl std::ops::Neg for V4 {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        Self {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+            w: -self.w,
+        }
+    }
+}
+
+impl std::ops::Add for V4 {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+            w: self.w + other.w,
+        }
+    }
+}
+
+impl std::ops::AddAssign for V4 {
+    fn add_assign(&mut self, rhs: Self) {
+        *self = *self + rhs
+    }
+}
+
+impl std::ops::Sub for V4 {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+            w: self.w - other.w,
+        }
+    }
+}
+
+impl std::ops::SubAssign for V4 {
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = *self - rhs
+    }
+}
+
+impl std::ops::Mul<f32> for V4 {
+    type Output = Self;
+
+    fn mul(self, scalar: f32) -> Self::Output {
+        Self {
+            x: self.x * scalar,
+            y: self.y * scalar,
+            z: self.z * scalar,
+            w: self.w * scalar,
+        }
+    }
+}
+
+impl std::ops::MulAssign<f32> for V4 {
+    fn mul_assign(&mut self, scalar: f32) {
+        *self = *self * scalar
+    }
+}
+
+impl std::ops::Div<f32> for V4 {
+    type Output = Self;
+
+    fn div(self, scalar: f32) -> Self::Output {
+        Self {
+            x: self.x / scalar,
+            y: self.y / scalar,
+            z: self.z / scalar,
+            w: self.w / scalar,
+        }
+    }
+}
+
+impl std::ops::DivAssign<f32> for V4 {
+    fn div_assign(&mut self, scalar: f32) {
+        *self = *self / scalar
+    }
+}
+
 impl V4 {
-    pub fn new(x: f32, y: f32, z: f32, w: f32) -> V4 {
-        V4 { x, y, z, w }
+    /// Construct a new [`V4`], using the provided values.
+    pub const fn new(x: f32, y: f32, z: f32, w: f32) -> Self {
+        Self { x, y, z, w }
     }
 
-    pub fn unit_x() -> V4 {
-        V4 {
-            x: 1.0,
-            y: 0.0,
-            z: 0.0,
-            w: 0.0,
-        }
+    /// A zero vector.
+    pub const fn zero() -> Self {
+        Self::new(0.0, 0.0, 0.0, 0.0)
     }
 
-    pub fn unit_y() -> V4 {
-        V4 {
-            x: 0.0,
-            y: 1.0,
-            z: 0.0,
-            w: 0.0,
-        }
+    /// A unit vector in the `x` direction.
+    pub const fn unit_x() -> Self {
+        Self::new(1.0, 0.0, 0.0, 0.0)
     }
 
-    pub fn unit_z() -> V4 {
-        V4 {
-            x: 0.0,
-            y: 0.0,
-            z: 1.0,
-            w: 0.0,
-        }
+    /// A unit vector in the `y` direction.
+    pub const fn unit_y() -> Self {
+        Self::new(0.0, 1.0, 0.0, 0.0)
     }
 
-    pub fn unit_w() -> V4 {
-        V4 {
-            x: 0.0,
-            y: 0.0,
-            z: 0.0,
-            w: 1.0,
-        }
+    /// A unit vector in the `z` direction.
+    pub const fn unit_z() -> Self {
+        Self::new(0.0, 0.0, 1.0, 0.0)
+    }
+
+    /// A unit vector in the `w` direction.
+    pub const fn unit_w() -> Self {
+        Self::new(0.0, 0.0, 0.0, 1.0)
+    }
+
+    /// Create a [`V3`], dropping the `w` value.
+    pub const fn truncate(self) -> V3 {
+        V3::new(self.x, self.y, self.z)
+    }
+
+    /// Returns the dot product of the vector and `other`.
+    pub fn dot(self, other: Self) -> f32 {
+        self.x * other.x + self.y * other.y + self.z * other.z + self.w * other.w
     }
 }
